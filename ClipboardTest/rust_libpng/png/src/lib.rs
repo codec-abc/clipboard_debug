8b74e1b657525cf3ef8ed6da52a24acfa758242f
cviot <viot.camille@outlook.com>
2021-01-18 14:42:32 +0100

    clean

--- ClipboardTest/rust_libpng/png/src/lib.rs
+++ ClipboardTest/rust_libpng/png/src/lib.rs
@@ -5,36 +5,6 @@ use libc::size_t;
 use image::ImageResult;
 use image::DynamicImage;
 use image::GenericImageView;
-use std::io::Write;
-use std::rc::Rc;
-use std::cell::RefCell;
-
-struct VecEncoder {
-    inner_vec: Rc<RefCell<Vec::<u8>>>
-}
-
-
-impl VecEncoder {
-    fn new(inner_vec: Rc<RefCell<Vec::<u8>>>) -> VecEncoder {
-        VecEncoder {
-            inner_vec
-        }
-    }
-}
-
-
-impl Write for VecEncoder {
-    fn write(&mut self, content_to_write: &[u8]) -> std::result::Result<usize, std::io::Error> { 
-        let mut borrow = self.inner_vec.borrow_mut();
-        borrow.write(content_to_write)
-    }
-
-    fn flush(&mut self) -> std::result::Result<(), std::io::Error> { 
-        let mut borrow = self.inner_vec.borrow_mut();
-        borrow.flush()
-    }
- 
-}
 
 #[no_mangle]
 pub unsafe extern "C" fn open_image(
@@ -58,7 +28,6 @@ pub unsafe extern "C" fn open_image(
                     let mut as_vec: Vec<u8> = raw.to_vec();
                     let length = as_vec.len();
                     let ptr: *mut u8 = as_vec.as_mut_ptr();
-                    //*len = length;
                     *width = image_decoded.width() as usize;
                     *height = image_decoded.height() as usize;
                     *out_ptr = ptr;
@@ -92,9 +61,8 @@ pub unsafe extern "C" fn encode_image_as_png(
             let decoded_result: ImageResult<DynamicImage> = image.decode();
             match decoded_result {
                 Ok(image_decoded) => {
-                    let buffer = Rc::new(RefCell::new(Vec::<u8>::new()));
-                    let vec_encoder = VecEncoder::new(buffer.clone());
-                    let png_encoder = image::codecs::png::PngEncoder::new(vec_encoder);
+                    let mut buffer = Vec::<u8>::new();
+                    let png_encoder = image::codecs::png::PngEncoder::new(&mut buffer);
                     let rgba = image_decoded.to_rgba8();
                     let raw: &[u8] = &(*rgba.into_raw());
                     
@@ -111,8 +79,7 @@ pub unsafe extern "C" fn encode_image_as_png(
                             return 0;
                         }
                         Ok(_) => {
-                            let borrow = buffer.borrow();
-                            let mut clone = borrow.clone();
+                            let mut clone = buffer.clone();
                             let length = clone.len();
                             println!("image encoded successfully on {} bytes", length);
                             let ptr: *mut u8 = clone.as_mut_ptr();
